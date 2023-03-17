c83476ce0bbcc13b8a34d951673032dc9b684c03
MarEnci <128103128+MarEnci@users.noreply.github.com>
2023-03-17 17:39:42 -0500

    Delete dias 3 directory

--- dias 3/Propiedades_strings.py
+++
@@ -1,8 +0,0 @@
-poema = '''Mil pequeños peces blancos
-como si hirbiera 
-el color del agua'''
-print(poema)
-print('agua' in poema)
-print('pez' in poema)
-print('Miles' not in poema)
-print(len(poema))
--- dias 3/formatear_cadenas1.py
+++
@@ -1,9 +0,0 @@
-puntos_anteriores = 875
-puntos_nuevos = 350
-x = puntos_anteriores
-y = puntos_nuevos
-print(f'Has ganado {y} puntos! En total, acumulas {x+y} puntos'.format(x,y,x+y))
-
-x = 874
-y = 27
-print(f'{x//y}')
